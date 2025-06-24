30c48f0383985ba8314baf00d7779dc0537f17a8
GYSLO-GK <gyslo.gn@gmail.com>
2025-06-24 20:06:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var a = document.getElementById("a")
-var b = document.getElementById("b")
-var c = document.getElementById("c")
-
-function check(a, b) {
-  if (a != "nouma" || b != "Lyla, Twilightsworn Enchantress"){
-    c.innerHTML="Wrong"
+function buttonConditions() {
+  var x = document.getElementById("b").value;
+  var y = document.getElementById("y");
+  if (x === "Lyla, Twilightsworn Enchantress") {
+    y.style.display = "block";
   }
-  
+  else
+  {
+    y.style.display = "none";
+  }
 }
