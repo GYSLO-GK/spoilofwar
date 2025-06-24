4c202775ce10cb19ebe9680dc14913fe69b917a5
GYSLO-GK <gyslo.gn@gmail.com>
2025-06-24 13:27:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = document.getElementById("a")
+var b = document.getElementById("b")
+var c = document.getElementById("c")
+
+function check(a, b) {
+  if (a != "nouma" || b != "Lyla, Twilightsworn Enchantress"){
+    c.innerHTML="Wrong"
+  }
+  
+}
