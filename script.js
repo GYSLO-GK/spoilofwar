8943dace7aa412315c17381840c6c760086e636a
GYSLO-GK <gyslo.gn@gmail.com>
2025-06-28 18:34:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function buttonConditions() {
   var x = document.getElementById("b").value;
   var y = document.getElementById("y");
-  alert(x);
   if (x === "Lyla, Twilightsworn Enchantress") {
     y.style.display = "block";
   }
